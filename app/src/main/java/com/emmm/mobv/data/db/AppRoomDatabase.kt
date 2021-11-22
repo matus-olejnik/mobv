eb4b3309ae6a4743dd8b8e75b6e843890bcca957
Matus Olejnik <matus.olejnik@gmail.com>
2021-11-22 11:20:47 +0100

    feat: create TransactionItem - new table

--- app/src/main/java/com/emmm/mobv/data/db/AppRoomDatabase.kt
+++ app/src/main/java/com/emmm/mobv/data/db/AppRoomDatabase.kt
@@ -5,11 +5,12 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.emmm.mobv.data.db.model.ContactItem
+import com.emmm.mobv.data.db.model.TransactionItem
 import com.emmm.mobv.data.db.model.UserAccountItem
 
 @Database(
-    entities = [ContactItem::class, UserAccountItem::class],
-    version = 4,
+    entities = [ContactItem::class, UserAccountItem::class, TransactionItem::class],
+    version = 6,
     exportSchema = false
 )
 abstract class AppRoomDatabase : RoomDatabase() {
