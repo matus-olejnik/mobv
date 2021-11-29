26c6776e0c80dc7e3fd149db25eb59727ce70c39
Matus Olejnik <matus.olejnik@gmail.com>
2021-11-29 12:44:18 +0100

    fix: double back button click to exit

--- app/src/main/java/com/emmm/mobv/BaseActivity.kt
+++ app/src/main/java/com/emmm/mobv/BaseActivity.kt
@@ -1,23 +1,8 @@
 package com.emmm.mobv
 
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 open class BaseActivity : AppCompatActivity() {
 
-    private var doubleBackToExitPressedOnce = false
 
-    override fun onBackPressed() {
-        if (doubleBackToExitPressedOnce) {
-            super.onBackPressed()
-            return
-        }
-
-        this.doubleBackToExitPressedOnce = true
-        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
-
-        Handler(Looper.getMainLooper()).postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
-    }
 }
--- app/src/main/java/com/emmm/mobv/MainActivity.kt
+++ app/src/main/java/com/emmm/mobv/MainActivity.kt
@@ -1,19 +1,44 @@
 package com.emmm.mobv
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : BaseActivity() {
 
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
         val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        bottomNavigationView.selectedItemId
 
+    }
+
+    private var doubleBackToExitPressedOnce = false
+
+    override fun onBackPressed() {
+        if (bottomNavigationView.selectedItemId == R.id.main_fragment) {
+            if (doubleBackToExitPressedOnce) {
+                finish()
+                return
+            }
+
+            this.doubleBackToExitPressedOnce = true
+            Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+
+            Handler(Looper.getMainLooper()).postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
+        } else {
+            doubleBackToExitPressedOnce = false
+            super.onBackPressed()
+        }
     }
 }
